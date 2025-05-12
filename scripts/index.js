815a9cfa40cfd2cc20d56cb335e3edd141910ed6
zumassh <stasy.sl@yandex.ru>
2025-05-12 14:45:02 +0300

    initial commit before fisrt review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+// Функция создания карточки
+function createCard(name, link, removeFunction) {
+    const card = cardTemplate.cloneNode(true);
+    const cardImg = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const deleteButton = card.querySelector('.card__delete-button');
+    cardImg.src = link;
+    cardImg.alt = name;
+    cardTitle.textContent = name;
+    deleteButton.addEventListener('click', function(evt) {
+        removeFunction(evt);
+    });
+    return card;
+}
+// Функция удаления карточки
+function removeCard(evt) {
+    const currentCard = evt.target.closest('.card');
+    currentCard.remove();
+}
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(element => {
+    const newCard = createCard(element.name, element.link, removeCard);
+    cardsList.append(newCard);
+});
